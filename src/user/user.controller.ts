1ea3ddc7e76bcb051fb1373374f79bf6a669c5db
Himanshu Taneja <Himanshu.Taneja@health.nsw.gov.au>
2024-09-26 14:02:14 +1000

    add controller functions for add, update, delete user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, NotFoundException, Post, Put, Query, Res } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -9,5 +10,42 @@ export class UserController {
     async getUsers(@Res() res) {
         const users = await this.userService.getUsers()
         return res.status(HttpStatus.OK).json(users)
+    }
+
+    @Post()
+    async addUser(@Res() res, @Body() createUserDTO: CreateUserDTO) {
+        const user = await this.userService.addUser(createUserDTO);
+        return res.status(HttpStatus.OK).json({
+            message: 'User record has been created successfully!',
+            user,
+        });
+    }
+
+    @Put()
+    async editUser(
+        @Res() res,
+        @Query('userID') userID,
+        @Body() createUserDTO: CreateUserDTO,
+    ) {
+        const user = await this.userService.editUser(userID, createUserDTO);
+        if (!user) {
+            throw new NotFoundException('User does not exist.');
+        }
+        return res.status(HttpStatus.OK).json({
+            message: 'User record has been successfully updated',
+            user,
+        });
+    }
+
+    @Delete()
+    async deleteUser(@Res() res, @Query('userID') userID) {
+        const user = await this.userService.deleteUser(userID);
+        if (!user) {
+            throw new NotFoundException('User does not exist.');
+        }
+        return res.status(HttpStatus.OK).json({
+            message: 'User has been deleted!',
+            user,
+        });
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -9,5 +10,18 @@ export class UserService {
 
     async getUsers(): Promise<User[]> {
         return await this.userModel.find().exec();
+    }
+
+    async addUser(createUserDTO: CreateUserDTO): Promise<User> {
+        const user = await new this.userModel(createUserDTO);
+        return user.save();
+    }
+
+    async editUser(userID, createPostDTO: CreateUserDTO): Promise<User> {
+        return await this.userModel.findByIdAndUpdate(userID, createPostDTO, { new: true });
+    }
+
+    async deleteUser(userID): Promise<any> {
+        return await this.userModel.findByIdAndDelete(userID);
     }
 }
