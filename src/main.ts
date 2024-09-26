a4d5521af79e04435b47e030cc6cfc6f192e1586
Himanshu Taneja <Himanshu.Taneja@health.nsw.gov.au>
2024-09-26 14:13:14 +1000

    catch all unhandled errors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { AllExceptionsFilter } from './all-exceptions-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+  
   const configService: ConfigService = app.get(ConfigService);
   await app.listen(configService.get<number>('port'));
 }
